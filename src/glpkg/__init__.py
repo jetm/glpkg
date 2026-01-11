5bd9cf94d6267be8bd1d29ecd0b5dc3359dcf4b8
Javier Tia <javier.tia@linaro.org>
2026-01-11 14:49:50 -0600

    Bump version: 0.1.0 → 0.1.1

--- src/glpkg/__init__.py
+++ src/glpkg/__init__.py
@@ -1,6 +1,6 @@
 """glpkg - GitLab Generic Package Upload Tool."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Export key models and exceptions for convenience
 from .duplicate_detector import DuplicateDetector
